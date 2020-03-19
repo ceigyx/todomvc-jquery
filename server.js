b0e60a0596c0fe832ee53896b5a7812a652c9134
ceigyx <aklypse_ceigyx@live.ca>
2020-03-19 05:37:16 -0400

    added selectable view with node argument, removed jQuery from init and bindEvents functions, polyfilled EventTarget with a chainable on function to maintain mvc

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+  response.sendFile(__dirname + `/views/${process.argv[2]}.html`);
 });
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(1337, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
