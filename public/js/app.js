68e9bf7d1e1bd4b5e6ea795e6cea97255df4e847
ceigyx <aklypse_ceigyx@live.ca>
2020-06-26 08:27:04 -0400

    swapped to no methods app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,196 +1,227 @@
 /*global jQuery, Handlebars, Router */
-jQuery(function ($) { //on readdy, do following
-	'use strict';
+jQuery(function($) {
+    "use strict";
+  
+    Handlebars.registerHelper("eq", function(a, b, options) {
+      return a === b ? options.fn(this) : options.inverse(this);
+    });
+  
+    var ENTER_KEY = 13;
+    var ESCAPE_KEY = 27;
+  
+    function uuid() {
+      /*jshint bitwise:false */
+      var i, random;
+      var uuid = "";
+  
+      for (i = 0; i < 32; i++) {
+        random = (Math.random() * 16) | 0;
+        if (i === 8 || i === 12 || i === 16 || i === 20) {
+          uuid += "-";
+        }
+        uuid += (i === 12 ? 4 : i === 16 ? (random & 3) | 8 : random).toString(
+          16
+        );
+      }
+  
+      return uuid;
+    }
+  
+    function pluralize(count, word) {
+      return count === 1 ? word : word + "s";
+    }
+  
+    function store(namespace, data) {
+      if (arguments.length > 1) {
+        return localStorage.setItem(namespace, JSON.stringify(data));
+      } else {
+        var store = localStorage.getItem(namespace);
+        return (store && JSON.parse(store)) || [];
+      }
+    }
+  
+    //Util functions ok
+  
 
-	Handlebars.registerHelper('eq', function (a, b, options) {
-		return a === b ? options.fn(this) : options.inverse(this);
-	});
+    //since app object gone, need to declare variables.
+    var todos, todoTemplate, footerTemplate, filter;
 
-	var ENTER_KEY = 13;
-	var ESCAPE_KEY = 27;
 
-	var util = {
-		 uuid: function () {
-			/*jshint bitwise:false */
-			var i, random;
-			var uuid = '';
-
-			for (i = 0; i < 32; i++) {
-				random = Math.random() * 16 | 0;
-				if (i === 8 || i === 12 || i === 16 || i === 20) {
-					uuid += '-';
-				}
-				uuid += (i === 12 ? 4 : (i === 16 ? (random & 3 | 8) : random)).toString(16);
-			}
-
-			return uuid;
-		},
-		pluralize: function (count, word) {
-			return count === 1 ? word : word + 's';
-		},
-		store: function (namespace, data) {
-			if (arguments.length > 1) {
-				return localStorage.setItem(namespace, JSON.stringify(data));
-			} else {
-				var store = localStorage.getItem(namespace);
-				return (store && JSON.parse(store)) || [];
-			}
-		}
-	};
-
-	var App = {
-		init: function () {
-			this.todos = util.store('todos-jquery');
-			this.todoTemplate = Handlebars.compile($('#todo-template').html());
-			this.footerTemplate = Handlebars.compile($('#footer-template').html());
-			this.bindEvents();
-
-			new Router({
-				'/:filter': function (filter) {
-					this.filter = filter;
-					this.render();
-				}.bind(this)
-			}).init('/all');
-		},
-		bindEvents: function () {
-			$('#new-todo').on('keyup', this.create.bind(this));
-			$('#toggle-all').on('change', this.toggleAll.bind(this));
-			$('#footer').on('click', '#clear-completed', this.destroyCompleted.bind(this));
-			$('#todo-list')
-				.on('change', '.toggle', this.toggle.bind(this))
-				.on('dblclick', 'label', this.editMode.bind(this))
-				.on('keyup', '.edit', this.editModeKeyup.bind(this))
-				.on('focusout', '.edit', this.updateTodo.bind(this))
-				.on('click', '.destroy', this.destroy.bind(this));
-		},
-		render: function () {
-			var todos = this.getFilteredTodos();
-			$('#todo-list').html(this.todoTemplate(todos));
-			$('#main').toggle(todos.length > 0);
-			$('#toggle-all').prop('checked', this.getActiveTodos().length === 0);
-			this.renderFooter();
-			$('#new-todo').focus();
-			util.store('todos-jquery', this.todos);
-		},
-		renderFooter: function () {
-			var todoCount = this.todos.length;
-			var activeTodoCount = this.getActiveTodos().length;
-			var template = this.footerTemplate({
-				activeTodoCount: activeTodoCount,
-				activeTodoWord: util.pluralize(activeTodoCount, 'item'),
-				completedTodos: todoCount - activeTodoCount,
-				filter: this.filter
-			});
-
-			$('#footer').toggle(todoCount > 0).html(template);
-		},
-		toggleAll: function (e) {
-			var isChecked = $(e.target).prop('checked');
-
-			this.todos.forEach(function (todo) {
-				todo.completed = isChecked;
-			});
-
-			this.render();
-		},
-		getActiveTodos: function () {
-			return this.todos.filter(function (todo) {
-				return !todo.completed;
-			});
-		},
-		getCompletedTodos: function () {
-			return this.todos.filter(function (todo) {
-				return todo.completed;
-			});
-		},
-		getFilteredTodos: function () {
-			if (this.filter === 'active') {
-				return this.getActiveTodos();
-			}
-
-			if (this.filter === 'completed') {
-				return this.getCompletedTodos();
-			}
-
-			return this.todos;
-		},
-		destroyCompleted: function () {
-			this.todos = this.getActiveTodos();
-			this.filter = 'all';
-			this.render();
-		},
-		// accepts an element from inside the `.item` div and
-		// returns the corresponding index in the `todos` array
-		indexFromEl: function (el) {
-			var id = $(el).closest('li').data('id');
-			var todos = this.todos;
-			var i = todos.length;
-
-			while (i--) {
-				if (todos[i].id === id) {
-					return i;
-				}
-			}
-		},
-		create: function (e) {
-			var $input = $(e.target);
-			var val = $input.val().trim();
-
-			if (e.which !== ENTER_KEY || !val) {
-				return;
-			}
-
-			this.todos.push({
-				id: util.uuid(),
-				title: val,
-				completed: false
-			});
-
-			$input.val('');
-
-			this.render();
-		},
-		toggle: function (e) {
-			var i = this.indexFromEl(e.target);
-			this.todos[i].completed = !this.todos[i].completed;
-			this.render();
-		},
-		editMode: function (e) {
-			var $input = $(e.target).closest('li').addClass('editing').find('.edit');
-			$input.focus();
-		},
-		editModeKeyup: function (e) {
-			if (e.which === ENTER_KEY) {
-				e.target.blur();
-			}
-
-			if (e.which === ESCAPE_KEY) {
-				$(e.target).data('abort', true).blur();
-			}
-		},
-		updateTodo: function (e) {
-			var el = e.target;
-			var $el = $(el);
-			var val = $el.val().trim();
-
-			if (!val) {
-				this.destroy(e);
-				return;
-			}
-
-			if ($el.data('abort')) {
-				$el.data('abort', false);
-			} else {
-				this.todos[this.indexFromEl(el)].title = val;
-			}
-
-			this.render();
-		},
-		destroy: function (e) {
-			this.todos.splice(this.indexFromEl(e.target), 1);
-			this.render();
-		}
-	};
-
-	App.init();
-});
+    //change methods to function declarations and remove all "this" since everything should
+    //be visible in function scope.
+    function init() {
+      todos = store("todos-jquery");
+      todoTemplate = Handlebars.compile($("#todo-template").html());
+      footerTemplate = Handlebars.compile($("#footer-template").html());
+      bindEvents();
+  
+      new Router({
+        "/:newfilter": function(newfilter) {
+          filter = newfilter;
+          render();
+        }
+      }).init("/all");
+    }
+  
+    function bindEvents() {
+      $("#new-todo").on("keyup", create.bind());
+      $("#toggle-all").on("change", toggleAll.bind());
+      $("#footer").on("click", "#clear-completed", destroyCompleted.bind());
+      $("#todo-list")
+        .on("change", ".toggle", toggle.bind())
+        .on("dblclick", "label", edit.bind())
+        .on("keyup", ".edit", editKeyup.bind())
+        .on("focusout", ".edit", update.bind())
+        .on("click", ".destroy", destroy.bind());
+    }
+  
+    function render() {
+      var filteredTodos = getFilteredTodos();
+      $("#todo-list").html(todoTemplate(filteredTodos));
+      $("#main").toggle(filteredTodos.length > 0);
+      $("#toggle-all").prop("checked", getActiveTodos().length === 0);
+      renderFooter();
+      $("#new-todo").focus();
+      store("todos-jquery", filteredTodos);
+    }
+  
+    function renderFooter() {
+      var todoCount = todos.length;
+      var activeTodoCount = getActiveTodos().length;
+      var template = footerTemplate({
+        activeTodoCount: activeTodoCount,
+        activeTodoWord: pluralize(activeTodoCount, "item"),
+        completedTodos: todoCount - activeTodoCount,
+        filter: filter
+      });
+  
+      $("#footer")
+        .toggle(todoCount > 0)
+        .html(template);
+    }
+  
+    function toggleAll(e) {
+      var isChecked = $(e.target).prop("checked");
+  
+      todos.forEach(function(todo) {
+        todo.completed = isChecked;
+      });
+  
+      render();
+    }
+  
+    function getActiveTodos() {
+      return todos.filter(function(todo) {
+        return !todo.completed;
+      });
+    }
+  
+    function getCompletedTodos() {
+      return todos.filter(function(todo) {
+        return todo.completed;
+      });
+    }
+  
+    function getFilteredTodos() {
+      if (filter === "active") {
+        return getActiveTodos();
+      }
+  
+      if (filter === "completed") {
+        return getCompletedTodos();
+      }
+  
+      return todos;
+    }
+  
+    function destroyCompleted() {
+      todos = getActiveTodos();
+      filter = "all";
+      render();
+    }
+  
+    function indexFromEl(el) {
+      var id = $(el)
+        .closest("li")
+        .data("id");
+      var todosList = todos;
+      var i = todosList.length;
+  
+      while (i--) {
+        if (todosList[i].id === id) {
+          return i;
+        }
+      }
+    }
+  
+    function create(e) {
+      var $input = $(e.target);
+      var val = $input.val().trim();
+  
+      if (e.which !== ENTER_KEY || !val) {
+        return;
+      }
+  
+      todos.push({
+        id: uuid(),
+        title: val,
+        completed: false
+      });
+  
+      $input.val("");
+  
+      render();
+    }
+  
+    function toggle(e) {
+      var i = indexFromEl(e.target);
+      todos[i].completed = !todos[i].completed;
+      render();
+    }
+  
+    function edit(e) {
+      var $input = $(e.target)
+        .closest("li")
+        .addClass("editing")
+        .find(".edit");
+      $input.val($input.val()).focus();
+    }
+  
+    function editKeyup(e) {
+      if (e.which === ENTER_KEY) {
+        e.target.blur();
+      }
+  
+      if (e.which === ESCAPE_KEY) {
+        $(e.target)
+          .data("abort", true)
+          .blur();
+      }
+    }
+  
+    function update(e) {
+      var el = e.target;
+      var $el = $(el);
+      var val = $el.val().trim();
+  
+      if (!val) {
+        destroy(e);
+        return;
+      }
+  
+      if ($el.data("abort")) {
+        $el.data("abort", false);
+      } else {
+        todos[indexFromEl(el)].title = val;
+      }
+  
+      render();
+    }
+  
+    function destroy(e) {
+      todos.splice(indexFromEl(e.target), 1);
+      render();
+    }
+  
+    init();
+  });
